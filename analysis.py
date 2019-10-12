f8b46fed8a39f8f94abd5481d5bc7044c8a8d2f2
tajimamasa <pkpkpkpppp123@gmail.com>
2019-10-13 02:39:12 +0900

    update analysis

--- analysis.py
+++ analysis.py
@@ -35,88 +35,80 @@ class Application(Tkinter.Frame):
         self.bFrame.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.7)
 
         self.mFrame = Tkinter.Frame(self.master)
-        self.mFrame.place(relx=0, rely=0.7, relwidth=1, relheight=0.3)#, bg="red")
+        self.mFrame.place(relx=0, rely=0.7, relwidth=1, relheight=0.3)
         
     def create_widgets(self):
-        
-        self.hi_there = Tkinter.Button(self.bFrame,text='Reload',command=self.reflesh)
-        self.hi_there.pack(anchor=Tkinter.NW)
-        self.hi_there.place(relx=0, rely=0.75)
-        
         self.select = Tkinter.Button(self.bFrame, text="select/plot", command=self.select)
-        #self.select.pack(anchor=Tkinter.NW)
         self.select.place(relx=0.5, rely=0.75)
         
         self.file_list = Tkinter.Listbox(self.lFrame ,selectmode='browse')
         self.reflesh()
-        #self.file_list.pack(anchor=Tkinter.NW)
-        #self.scroll_bar =Tkinter.Scrollbar(self.lFrame, command=self.file_list.yview)
-        #self.scroll_bar.pack(side=Tkinter.RIGHT, fill="y")
         self.file_list.place(relx=0.05, rely=0.1,relheight=0.8,relwidth=0.8)
-        #self.file_list.config(yscrollcommand=self.scroll_bar.set)
 
     def create_checkbox(self):
         self.iv1 = Tkinter.IntVar()
         self.iv1.set(0)
-        Tkinter.Radiobutton(self.bFrame, text="memo", value=0, variable=self.iv1).place(relx=0., rely=0.15)
-        Tkinter.Radiobutton(self.bFrame, text="time_domain", value=1, variable=self.iv1).place(relx=0., rely=0.25)
-        Tkinter.Radiobutton(self.bFrame, text="frequency_domain", value=2, variable=self.iv1).place(relx=0., rely=0.35)
-        Tkinter.Radiobutton(self.bFrame, text="delay_clock", value=3, variable=self.iv1).place(relx=0., rely=0.45)
-        Tkinter.Radiobutton(self.bFrame, text="process", value=4, variable=self.iv1).place(relx=0., rely=0.55)
-        Tkinter.Radiobutton(self.bFrame, text="turn-by-turn", value=4, variable=self.iv1).place(relx=0., rely=0.65)
+        Tkinter.Radiobutton(self.bFrame, text="waveform", value=0, variable=self.iv1).place(relx=0., rely=0.15)
+        Tkinter.Radiobutton(self.bFrame, text="FFT", value=1, variable=self.iv1).place(relx=0., rely=0.25)
+        Tkinter.Radiobutton(self.bFrame, text="delay_clock", value=2, variable=self.iv1).place(relx=0., rely=0.35)
+        Tkinter.Radiobutton(self.bFrame, text="process", value=3, variable=self.iv1).place(relx=0., rely=0.45)
 
     def create_memobox(self):
         self.textField = Tkinter.Text(self.mFrame, relief="groove")
         #self.textField.pack(side='bottom',anchor=Tkinter.S)
-        self.textField.place(relx=0.1, rely=0.,relheight=0.8,relwidth=0.8)
-        self.textField.insert('insert','-----------Memo------------\n')
+        self.textField.place(relx=0.1, rely=0.,relheight=0.6,relwidth=0.8)
+        self.textField.insert('insert','-----------Log------------\n')
 
     def create_quit(self):
         self.quit = Tkinter.Button(self.mFrame, text="EXIT", command=self.master.destroy)
-        self.quit.place(relx=0.9, rely=0.8)
+        self.quit.place(relx=0.8, rely=0.7)
         
     def reflesh(self,path=data_path):
         self.data_path = path +'/'
         self.file_list.delete(0,'end')
         files = os.listdir(path)
-        self.file_list.insert(0, 'file_list: '+path)
+        self.file_list.insert(1, './')
+        self.file_list.insert(2, '../')
         for i in range(len(files)):
-            self.file_list.insert(i+1, '  '+files[i])
+            self.file_list.insert(i+3, '  '+files[i])
         #self.file_list.pack()
         
     def select(self):
         file_index = self.file_list.curselection()
         file_name = self.data_path+self.file_list.get(file_index[0]).replace('  ','')
         if not file_index: return
-        elif file_index[0] == 0 : self.reflesh()
         elif os.path.isdir(file_name):
             self.reflesh(path=file_name)
         elif '.dat' in file_name:
-            num = self.iv1.get()
-            if num != 0:
-                self.plot(file_name,num)
-            elif num == 0:
-                self.memo(file_name)
+            self.memo(file_name)
+            self.plot(file_name)
 
-    def plot(self,file_name,num):
+    def plot(self,file_name):
         vol = decode_wave.read_wave_file(file_name)
-        x = range(len(vol[0]))
-        fig,axes = plt.subplots(nrows=4,ncols=4)
+        num = len(vol[0])
+        num_range = 64*9
+        x = range(num_range)
         for ch in range(16):
-            axes[ch//4,ch%4].plot(x,vol[0],label=str(ch))
+            plt.text(num_range*1.1,2**13+2**14*ch,'ch '+str(ch))
+            [ plt.plot(x,vol[ch][i*num_range:(i+1)*num_range]+2**14*ch,c='black',linewidth=0.5) for i in range(int(num/num_range))]
+        plt.ylim(0,16*2**14)
+        plt.title(file_name)
         plt.show()
+            
 
     def memo(self,file_name):
-        file_name = file_name.replace('/data', '/memo')
         file_name = file_name.replace('.dat','.txt')
+        file_name = file_name.replace('wave_data','log')
+        file_name = file_name.replace('process_data','log')
+        file_name = file_name.replace('wave','log')
+        file_name = file_name.replace('process','log')
+        self.textField.delete('1.0','end')
         if os.path.exists(file_name):
-            command = ['cat',file_name]
-            p = subprocess.Popen(command, stdout=subprocess.PIPE)
-            out = p.stdout.read()
-            self.textField.insert('insert',file_name)
+            fd = open(file_name,'r')
+            out = fd.read()
             self.textField.insert('insert',out)
         else:
-            self.textField.insert('insert','memo is not exist\n')
+            self.textField.insert('insert','Log is not exist\n')
 
 root = Tkinter.Tk()
 app = Application(master=root)
